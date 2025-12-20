a5d803ebef0c798fc944a85c2da14290828298e7
Sreekar Nutulapati <sreekarnv1@gmail.com>
2025-12-20 08:36:58 +0530

    :key: feat(auth): add JWT access token creation and validation

--- fastauth/settings.py
+++ fastauth/settings.py
@@ -1,8 +1,14 @@
+from datetime import timedelta
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     database_url: str = "sqlite:///./fastauth.db"
+
+    jwt_secret_key: str = "CHANGE_ME_SUPER_SECRET"
+    jwt_algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
