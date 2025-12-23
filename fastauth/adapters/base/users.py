cb816efa5980b02c9de97cd6253584c51b8178c0
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2025-12-24 02:58:24 +0530

    ♻️ refactor(adapters): introduce storage adapter interfaces (#19)

---
+++ fastauth/adapters/base/users.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import uuid
+
+
+class UserAdapter(ABC):
+    @abstractmethod
+    def get_by_email(self, email: str):
+        ...
+
+    @abstractmethod
+    def get_by_id(self, user_id: uuid.UUID):
+        ...
+
+    @abstractmethod
+    def create_user(self, *, email: str, hashed_password: str):
+        ...
+
+    @abstractmethod
+    def mark_verified(self, user_id: uuid.UUID) -> None:
+        ...
