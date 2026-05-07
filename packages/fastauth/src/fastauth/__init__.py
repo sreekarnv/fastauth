67e2913d5cea5a37e0ed18814659a0cb88e7bcba
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-05-08 01:24:31 +0530

    🔖 bump version to 0.5.4

--- packages/fastauth/src/fastauth/__init__.py
+++ packages/fastauth/src/fastauth/__init__.py
@@ -1,5 +1,5 @@
 from fastauth.app import FastAuth
 from fastauth.config import FastAuthConfig, JWTConfig
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __all__ = ["FastAuth", "FastAuthConfig", "JWTConfig", "__version__"]
